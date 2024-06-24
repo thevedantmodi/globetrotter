54d52982806824d3fdde201c25c04decda5ad183
Vedant Modi <vedantmodi@gmail.com>
2024-06-24 20:40:59 +1000

    Add from tutorial

---
+++ front/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+// import { useState } from 'react';
+import Map from 'react-map-gl'
+
+function App () {
+  // const [viewport, setViewport] = useState({
+  //   width: 400,
+  //   height: 400,
+  //   latitude: 37.7577,
+  //   longitude: -122.4376,
+  //   zoom: 8
+  // })
+  return (
+    <div className='App'>
+      <Map
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+        initialViewState={{
+          longitude: -122.4,
+          latitude: 37.8,
+          zoom: 14
+        }}
+        style={{ width: '100vw', height: '100vh' }}
+        mapStyle='mapbox://styles/mapbox/streets-v9'
+      />
+    </div>
+  )
+}
+
+export default App
