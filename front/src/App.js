340a5deacaecc0db6b500a635805da7c4812a996
Vedant Modi <vedantmodi@gmail.com>
2024-07-17 14:44:15 -0400

    Add new

--- front/src/App.js
+++ front/src/App.js
@@ -3,13 +3,13 @@ import React from 'react'
 import Map from 'react-map-gl'
 
 function App () {
-  // const [viewport, setViewport] = useState({
-  //   width: 400,
-  //   height: 400,
-  //   latitude: 37.7577,
-  //   longitude: -122.4376,
-  //   zoom: 8
-  // })
+  const [viewport, setViewport] = useState({
+    width: 400,
+    height: 400,
+    latitude: 37.7577,
+    longitude: -122.4376,
+    zoom: 8
+  })
   return (
     <div className='App'>
       <Map
