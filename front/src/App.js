2cd4e28f8f37103e8da5bbbb4326cdc91f5a330e
Vedant Modi <vedantmodi@gmail.com>
2024-07-17 17:05:22 -0400

    Add new Marker

--- front/src/App.js
+++ front/src/App.js
@@ -1,27 +1,31 @@
 import React from 'react'
-// import { useState } from 'react';
-import Map from 'react-map-gl'
+import { useState } from 'react'
+import ReactMapGL, { Marker } from 'react-map-gl'
+// import { MapPin } from '@phosphor-icons/react'
 
 function App () {
-  const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
+  /*   let BLONG = 153.021072
+  let BLAT = -27.470125 */
+
+  const [viewState, setViewState] = useState({
+    latitude: -27.470125,
+    longitude: 153.021072,
+    zoom: 4
   })
+
   return (
     <div className='App'>
-      <Map
-        mapboxAccessToken={process.env.REACT_APP_MAPBOX}
-        initialViewState={{
-          longitude: -122.4,
-          latitude: 37.8,
-          zoom: 14
-        }}
+      <ReactMapGL
+        {...viewState}
+        onMove={evt => setViewState(evt.viewState)}
         style={{ width: '100vw', height: '100vh' }}
         mapStyle='mapbox://styles/mapbox/streets-v9'
-      />
+        mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+      >
+        <Marker longitude={153.021072} latitude={-27.470125}>
+          <h1>This is not centered</h1>
+        </Marker>
+      </ReactMapGL>
     </div>
   )
 }
