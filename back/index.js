5016307400d662e42243bf9835660f0405e5d574
Vedant Modi <vedantmodi@gmail.com>
2024-07-22 12:03:26 -0400

    Successful full stack connection!

--- back/index.js
+++ back/index.js
@@ -1,25 +1,26 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const dotenv = require("dotenv") /* Maybe not needed */
+const express = require('express')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv') /* Maybe not needed */
 
-const userRoute = require("./routes/users.js")
-const pinRoute = require("./routes/pins.js")
+const userRoute = require('./routes/users.js')
+const pinRoute = require('./routes/pins.js')
 
-const app = express();
+const app = express()
 
 dotenv.config() /* Maybe not needed */
 
 app.use(express.json())
 
-mongoose.connect(process.env.MONGO_URL).then(() => {
-    console.log("Successful connection")
-}).catch(err => console.log(err))
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log('Successful connection! to MongoDB')
+  })
+  .catch(err => console.log(err))
 
-app.use("/api/users", userRoute)
-app.use("/api/pins", pinRoute)
+app.use('/api/users', userRoute)
+app.use('/api/pins', pinRoute)
 
-app.listen(8000, () => {
-    console.log("So the backend begins Here we go!")
+app.listen(8800, () => {
+  console.log('So the backend begins Here we go!')
 })
-
-
--- front/src/App.js
+++ front/src/App.js
@@ -1,17 +1,33 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import ReactMapGL, { Marker, Popup } from 'react-map-gl'
-import { MapPin, AirplaneTakeoff } from '@phosphor-icons/react'
+import { Circle, AirplaneTakeoff } from '@phosphor-icons/react'
 import axios from 'axios'
+import TimeAgo from 'javascript-time-ago'
+import en from 'javascript-time-ago/locale/en'
+import ReactTimeAgo from 'react-time-ago'
+import DeckGL from '@deck.gl/react'
+import { MapViewState, FlyToInterpolator } from '@deck.gl/core'
 import 'mapbox-gl/dist/mapbox-gl.css'
 import './App.css'
 
+TimeAgo.addDefaultLocale(en)
+
 function App () {
+  const current_user = 'vedantmodi'
+
+  //   const [viewState, setViewState] = useState({
+  //     latitude: -27.470125,
+  //     longitude: 153.021072,
+  //     zoom: 14
+  //   })
+
   const [viewState, setViewState] = useState({
-    latitude: -27.470125,
-    longitude: 153.021072,
-    zoom: 4
+    latitude: 30,
+    longitude: 0,
+    zoom: 1.5
   })
+
+  const [current_place, setCurrentPlace] = useState(null)
 
   const [pins, setPins] = useState([])
 
@@ -27,6 +43,23 @@ function App () {
     getPins()
   }, [])
 
+  const onMarkerClick = (id, lat, long) => {
+    setCurrentPlace(id)
+    setViewState({
+      ...viewState,
+      latitude: lat,
+      longitude: long,
+      transitionDuration: 1000
+    })
+  }
+
+  const onMapClick = e => {
+    const [lat, long] = [e.lngLat.lat, e.lngLat.lng]
+    setNewPlace({ lat, long })
+  }
+
+  const [newPlace, setNewPlace] = useState(null)
+
   return (
     <div className='App'>
       <ReactMapGL
@@ -35,33 +68,60 @@ function App () {
         style={{ width: '100vw', height: '100vh' }}
         mapStyle='mapbox://styles/mapbox/streets-v9'
         mapboxAccessToken={process.env.REACT_APP_MAPBOX}
+        onDblClick={onMapClick}
+        projection='globe'
       >
         {pins.map(p => (
           <div>
             <Marker
-              longitude={p.long}
+              longitude={p.lon}
               latitude={p.lat}
               offsetLeft={-20}
               offsetTop={-10}
             >
-              <MapPin
+              <Circle
                 weight='fill'
-                style={{ fontSize: viewState.zoom * 10, color: 'black' }}
+                style={{
+                  fontSize: viewState.zoom * 2.2,
+                  color: p.username === current_user ? 'red' : 'black',
+                  cursor: 'pointer'
+                }}
+                onClick={() => onMarkerClick(p._id, p.lat, p.lon)}
               />
             </Marker>
-            <Popup
-              longitude={p.long}
-              latitude={p.lat}
-              closeButton={true}
-              closeOnClick={false}
-              anchor='right'
-            >
-              <div className='info-card'>
-                <h2>
-                  Brisbane, AU <AirplaneTakeoff size={32} />
-                </h2>
-              </div>
-            </Popup>
+            {/* If the current place is desired, show the popup */}
+            {p._id === current_place && (
+              <Popup
+                longitude={p.lon}
+                latitude={p.lat}
+                closeButton={true}
+                closeOnClick={false}
+                anchor='right'
+                onClose={() =>
+                  setCurrentPlace(null)
+                } /* Clear after we're done */
+              >
+                <div className='info-card'>
+                  <h2>
+                    {p.title} <AirplaneTakeoff size={32} />
+                  </h2>
+                  <p>
+                    Post was made <ReactTimeAgo date={p.createdAt} />
+                  </p>
+                </div>
+              </Popup>
+            )}
+
+            {newPlace && (
+              <Popup
+                longitude={newPlace.long}
+                latitude={newPlace.lat}
+                closeButton={true}
+                closeOnClick={false}
+                anchor='right'
+                onClose={() => setNewPlace(null)} /* Clear after we're done */
+              />
+            )}
           </div>
         ))}
       </ReactMapGL>
