a34207eb1ed966bf5d3b5fe0ce6ba0b0959b97f5
Vedant Modi <vedantmodi@gmail.com>
2024-07-23 17:32:27 -0400

    Add airports JSON data

---
+++ data-treatment/csv-to-json.py
@@ -0,0 +1,25 @@
+##############################################################
+#
+#                     csv-to-json.py
+#
+#
+#     Authored by Vedant Modi (vedantmodi.com)
+#     23 Jul 2024
+#
+#
+#     Script to convert mborsetti's airportsdata repository
+#     from CSV to JSON. Dataset is used under the MIT license,
+#     and can be found at https://github.com/mborsetti/airportsdata
+#
+##############################################################
+
+import csv
+import json
+import os
+
+with open("airports.csv") as infile:
+    reader = csv.DictReader(infile)
+    data = [row for row in reader]
+    
+with open("airports.json", 'w') as outfile:
+    json.dump(data, outfile)
