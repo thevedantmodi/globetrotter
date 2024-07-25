347f1397f5ed75d76ff39c0522d139fccf118f1e
Vedant Modi <vedantmodi@gmail.com>
2024-07-25 11:25:28 -0400

    Update to parse airports as separate GeoJSON features

--- data-treatment/json-to-geojson.py
+++ data-treatment/json-to-geojson.py
@@ -17,25 +17,32 @@ import os
 URL = "airports.json"
 PATH = "/Users/vedantmodi/Desktop/dev-work.nosync/closed-flights/data-treatment/"
 OUT_FILE = "airports.geo.json"
+OUT_DIR = "airports-geo"
+
+# Create airports-geo directory to place output in
+os.makedirs(os.path.join(PATH, OUT_DIR), exist_ok=True)
 
 # Each Feature is a separate airport, contain in a FeatureCollection
-
 with open(os.path.join(PATH, URL)) as f:
     parsed = json.load(f)
 
-    geoJSON = {
-        "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [line["lon"], line["lat"]],
-                },
-                "properties": line,
-            }
-            for line in parsed
-        ],
-    }
-    with open(os.path.join(PATH, OUT_FILE), "w") as o:
-        json.dump(geoJSON, o)
+    for line in parsed:
+        geoJSON = {
+            "type": "FeatureCollection",
+            "features": [
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [line["lon"], line["lat"]],
+                    },
+                    "properties": line,
+                }
+            ],
+        }
+        name = line["icao"] + ".json"
+        with open(
+            os.path.join(PATH, OUT_DIR, name),
+            "w",
+        ) as o:
+            json.dump(geoJSON, o)
