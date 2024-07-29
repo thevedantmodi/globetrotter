32cb08ad2dd92735e2f780d5c8c6f2a4040491d7
Vedant Modi <vedantmodi@gmail.com>
2024-07-29 13:04:42 -0400

    Swapped files, created new ranking file

---
+++ data-treatment/airports-list-creator.py
@@ -0,0 +1,60 @@
+##############################################################
+#
+#                     airports-list-creator.py
+#
+#
+#     Authored by Vedant Modi (vedantmodi.com)
+#     29 Jul 2024
+#
+#
+#     Create list of airports in "popularity" order.
+#     Ranking of airports by "small", "medium", "large"
+#
+##############################################################
+
+
+import os
+
+import pandas
+
+path = "./flight-data"
+
+# Read into pandas dataframe
+# Count the number of occurrences of each origin, destination
+# Save to dictionary
+
+
+def print_codes(filename: str):
+    origins = dict()
+    destinations = dict()
+
+    df = pandas.read_csv(filename)
+
+    for code in df["origin"]:
+        try:
+            origins[code] = origins[code] + 1
+        except KeyError:
+            # First occurrence
+            origins.update({code: 1})
+    for code in df["destination"]:
+        try:
+            destinations[code] = destinations[code] + 1
+        except KeyError:
+            # First occurrence
+            destinations.update({code: 1})
+
+    origins = sorted(origins.items(), key=(lambda x: x[1]), reverse=True)
+    destinations = sorted(destinations.items(), key=(lambda x: x[1]), reverse=True)
+
+    with open("origins.csv", "a") as origin_file:
+        for origin in origins:
+            print(origin[0], end=",\n", file=origin_file)
+
+    with open("destinations.csv", "a") as destination_file:
+        for destination in destinations:
+            print(destination[0], end=",\n", file=destination_file)
+
+
+for root, dirs, file in os.walk(path):
+    for f in file:
+        print_codes(os.path.join(root, f))
