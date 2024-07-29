6fce341fa0016c5d08d44711eb70a5de5c8df463
Vedant Modi <vedantmodi@gmail.com>
2024-07-29 13:00:28 -0400

    Finish airports ranker

--- data-treatment/airports-ranker.py
+++ data-treatment/airports-ranker.py
@@ -8,47 +8,38 @@ path = "./flight-data"
 # Count the number of occurrences of each origin, destination
 # Save to dictionary
 
-origins = dict()
-destinations = dict()
+
+def print_codes(filename: str):
+    origins = dict()
+    destinations = dict()
+
+    df = pandas.read_csv(filename)
+
+    for code in df["origin"]:
+        try:
+            origins[code] = origins[code] + 1
+        except KeyError:
+            # First occurrence
+            origins.update({code: 1})
+    for code in df["destination"]:
+        try:
+            destinations[code] = destinations[code] + 1
+        except KeyError:
+            # First occurrence
+            destinations.update({code: 1})
+
+    origins = sorted(origins.items(), key=(lambda x: x[1]), reverse=True)
+    destinations = sorted(destinations.items(), key=(lambda x: x[1]), reverse=True)
+
+    with open("origins.csv", "a") as origin_file:
+        for origin in origins:
+            print(origin[0], end=",\n", file=origin_file)
+
+    with open("destinations.csv", "a") as destination_file:
+        for destination in destinations:
+            print(destination[0], end=",\n", file=destination_file)
 
 
-def read_all_csv():
-    def get_files():
-        files = []
-        for root, dirs, file in os.walk(path):
-            for f in file:
-                filename = os.path.join(root, f)
-                files.append(filename)
-
-        return files
-
-    dfs = [pandas.read_csv(file) for file in get_files()]
-
-    return dfs
-
-
-df = pandas.read_csv("./flight-data/flightlist_20190101_20190131.csv")
-for code in df["origin"]:
-    try:
-        origins[code] = origins[code] + 1
-    except KeyError:
-        # First occurrence
-        origins.update({code: 1})
-
-for code in df["destination"]:
-    try:
-        destinations[code] = destinations[code] + 1
-    except KeyError:
-        # First occurrence
-        destinations.update({code: 1})
-
-origins = sorted(origins.items(), key=(lambda x: x[1]), reverse=True)
-destinations = sorted(destinations.items(), key=(lambda x: x[1]), reverse=True)
-
-with open("origins.csv", "a") as origin_file:
-    for origin in origins:
-        print(origin, end=",\n", file=origin_file)
-
-with open("destinations.csv", "a") as destination_file:
-    for destination in destinations:
-        print(destination, end=",\n", file=destination_file)
+for root, dirs, file in os.walk(path):
+    for f in file:
+        print_codes(os.path.join(root, f))
