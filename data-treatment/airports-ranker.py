a5d24af1f86fb74e90c729d857452f610ed24359
Vedant Modi <vedantmodi@gmail.com>
2024-07-29 18:01:24 -0400

    Unsuccessful update to mongodb, must update model

--- data-treatment/airports-ranker.py
+++ data-treatment/airports-ranker.py
@@ -12,8 +12,43 @@
 #
 ##############################################################
 
-ORIGINS = "origins.csv"
-DESTINATIONS = "destinations.csv"
+import csv
 
-with open(ORIGINS) as o:
-    
+FILE = "origins.csv"
+OUTPUT = "airports-ranked.csv"
+# FILE = "destinations.csv"
+
+# Create fields code, size
+
+LARGE_BOUND = 300
+MEDIUM_BOUND = 750
+SMALL_BOUND = 750
+
+# Output as CSV
+
+fields = ["icao", "size"]
+
+i = open(FILE, "r")
+o = open(OUTPUT, "w")
+
+
+cw = csv.writer(o)
+cw.writerow(fields)
+
+
+def ranker(index):
+    if index <= LARGE_BOUND:
+        return "large"
+    elif index <= MEDIUM_BOUND:
+        return "medium"
+    else:
+        return "small"
+
+
+for index, line in enumerate(i):
+    row = [line[:4], ranker(index)]
+    cw.writerow(row)
+
+
+i.close()
+o.close()
--- data-treatment/json-to-geojson.py
+++ data-treatment/json-to-geojson.py
@@ -14,10 +14,15 @@
 import json
 import os
 
+import pandas
+
 URL = "airports.json"
 PATH = "/Users/vedantmodi/Desktop/dev-work.nosync/closed-flights/data-treatment/"
 OUT_FILE = "airports.geo.json"
 OUT_DIR = "airports-geo"
+SIZE_URL = "airports-ranked.csv"
+
+sizes_df = pandas.read_csv(SIZE_URL)
 
 # Create airports-geo directory to place output in
 os.makedirs(os.path.join(PATH, OUT_DIR), exist_ok=True)
@@ -27,6 +32,11 @@ with open(os.path.join(PATH, URL)) as f:
     parsed = json.load(f)
 
     for line in parsed:
+        # Inject new size field into airport
+        try:
+            line.update({"size": sizes_df.loc[line["icao"], "size"]})
+        except KeyError:
+            line.update({"size": "small"})
         geoJSON = {
             "type": "Feature",
             "geometry": {
