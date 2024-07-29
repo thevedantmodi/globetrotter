9d39f8eece1843da768421a0d6860eb67ef5ac61
Vedant Modi <vedantmodi@gmail.com>
2024-07-29 15:16:35 -0400

    Add origins.csv

--- data-treatment/airports-list-creator.py
+++ data-treatment/airports-list-creator.py
@@ -46,21 +46,19 @@ def print_codes(filename: str):
         except KeyError:
             # First occurrence
             destinations.update({code: 1})
-    # Must store this in a new variable, so other iterations can have a
-    # dictionary, too
-    sorted_origins = sorted(origins.items(), key=(lambda x: x[1]), reverse=True)
-    sorted_destinations = sorted(
-        destinations.items(), key=(lambda x: x[1]), reverse=True
-    )
 
-    with open("origins.csv", "a") as origin_file:
-        for origin in sorted_origins:
-            print(origin[0], end=",\n", file=origin_file)
-
-    with open("destinations.csv", "a") as destination_file:
-        for destination in sorted_destinations:
-            print(destination[0], end=",\n", file=destination_file)
 
 for root, dirs, file in os.walk(path):
     for f in file:
         print_codes(os.path.join(root, f))
+
+sorted_origins = sorted(origins.items(), key=(lambda x: x[1]), reverse=True)
+sorted_destinations = sorted(destinations.items(), key=(lambda x: x[1]), reverse=True)
+
+with open("origins.csv", "a") as origin_file:
+    for origin in sorted_origins:
+        print(origin[0], end=",\n", file=origin_file)
+
+with open("destinations.csv", "a") as destination_file:
+    for destination in sorted_destinations:
+        print(destination[0], end=",\n", file=destination_file)
