5c6903bb8666d19a49796395dac926bb54140a40
Vedant Modi <vedantmodi@gmail.com>
2024-08-12 17:36:20 -0400

    Add basic email/pwd/confirm-pwd functionality, yet to integrate zod

---
+++ front/src/components/SignUpField.tsx
@@ -0,0 +1,44 @@
+import { Input } from "react-daisyui"
+import { RefCallback } from "react";
+
+/* Types are happy now */
+interface SignUpFieldProps {
+    id: string,
+    label: string,
+    error?: string,
+    inputProps?: {
+        onChange?: (e: any) => unknown;
+        onBlur?: (e: any) => unknown;
+        ref?: any;
+        name?: string;
+        min?: string | number;
+        max?: string | number;
+        maxLength?: number;
+        minLength?: number;
+        pattern?: string;
+        required?: boolean;
+        disabled?: boolean;
+    },
+    type?: 'password' | 'text' | 'email',
+}
+
+export const SignUpField = (props: SignUpFieldProps) => {
+    return (
+        <div className="form-control w-full max-w-xs" >
+            <label htmlFor="email" className="label">
+                <span className="label-text">{props.label}</span>
+            </label>
+            <Input
+                color="ghost"
+                id={props.id}
+                type={props.type ?? "text"}
+                {...(props.inputProps ?? {})} /* Could be react-hook-form */
+            />
+            {props.error &&
+                <span className="label-text text-error">
+                    {props.error}
+                </span>}
+
+        </div>
+    )
+}
--- front/src/pages/SignUpPage.tsx
+++ front/src/pages/SignUpPage.tsx
@@ -1,28 +1,71 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { ModeToggle } from "../components/DarkModeButton";
 import { Input } from "react-daisyui"
+import { useForm, SubmitHandler } from "react-hook-form"
+import { SignUpField } from "../components/SignUpField";
+
+type Inputs = {
+    email: string
+    password: string
+    confirm_password: string
+}
 
 export default function SignUpPage() {
+    const {
+        register,
+        handleSubmit,
+        watch,
+        formState: { errors },
+    } = useForm<Inputs>()
+
+    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
+
+    const onValid = useCallback((data: any) => {
+        console.log(data)
+    }, [])
+
+
+    console.log(errors)
+
+
     return (
-        <div style={{
+        <form style={{
             display: 'flex',
             flexFlow: 'column',
             gap: 15,
             alignItems: 'center',
             justifyContent: 'center',
-            height: '100vh'
-        }}>
-            SignUpForm
-            <div className="form-control w-full max-w-xs">
-                <label htmlFor="email" className="label">
-                    <span className="label-text">Email Address</span>
-                </label>
-                <Input
-                    color="ghost"
-                    id="email"
-                    type="text"
-                />
-                <span className="label-text text-error">An error</span>
-            </div>
-        </div>
+            height: '100vh',
+        }}
+            onSubmit={handleSubmit(onValid)}
+
+        >
+
+            {/* <ModeToggle styles="" /> */}
+            <SignUpField
+                id="email"
+                label="Email address"
+                type="email"
+                inputProps={register("email", { required: "Must enter an email address" })}
+                error={errors.email?.message}
+            />
+            <SignUpField
+                id="password"
+                label="Password"
+                type="password"
+                inputProps={register("password", { required: "Must enter a password" })}
+                error={errors.password?.message}
+            />
+            <SignUpField
+                id="confirm-password"
+                label="Confirm Password"
+                type="password"
+                inputProps={register("confirm_password", { required: "Must confirm password" })}
+                error={errors.confirm_password?.message}
+            />
+            
+
+            <button>Submit</button>
+        </form>
     )
 }
