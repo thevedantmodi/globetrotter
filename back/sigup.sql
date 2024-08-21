bed8590e6e7bde47484b8990baab2f0faaf98ce4
Vedant Modi <vedantmodi@gmail.com>
2024-08-20 20:43:49 -0400

    Sucessfully added flight to table, attempt to add a user affiliation

--- back/sigup.sql
+++ back/sigup.sql
@@ -1,3 +1,2 @@
-INSERT INTO flights (dept_datetime)
-VALUES ('2003-04-12 04:05:06 America/New_York')
-RETURNING *;
+ALTER TABLE flights ADD COLUMN passenger_id
+ALTER TABLE flights ADD FOREIGN KEY (passenger_id) REFERENCES users(id);
